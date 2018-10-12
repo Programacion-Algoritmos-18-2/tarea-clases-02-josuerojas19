aaef18f10e75997bacf93aabe21851fc2f75b943
Josue Rojas <jsebastianrojas19@gmail.com>
2018-10-12 10:23:21 -0500

    Subiendo ejercio

---
+++ Ejercicio2/src/ejercicio2/Precio.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author Josue
+ */
+public class Precio {
+    double euros;
+    public double obtener(){
+        return 0;
+    }
+    public void agregar (double x){
+    
+    }    
+}
+
+class Principal{
+    public static void main (String []args){
+        Precio p=new Precio ();
+        System.out.println(p.euros);
+        
+    }
+}
